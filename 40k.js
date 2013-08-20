5651dde15061385f5f87344d973310a450528b97
christian <christian.hendelberg@igt.com>
2013-08-20 16:04:32 -0700

    movement and morale

--- 40k.js
+++ 40k.js
@@ -1,13 +1,13 @@
 var lastD6 = 0;
 
-var iterations = 1000;
+var iterations = 100;
 var winA = 0;
 var winB = 0;
 
 for (var x=0;x<iterations;x++) {
 	var a = new Object();
 	a.name = "orks";
-	a.Models = 12;
+	a.Models = 20;
 	a.Shots = 2;
 	a.Range = 18;
 	a.Armor = 6;
@@ -16,44 +16,111 @@ for (var x=0;x<iterations;x++) {
 	a.Cover = 0;
 	a.LD = 7;
 	a.Mob = true;
+	a.broken = false;
 	
 	a.STR = 4;
 	a.AP = 6;
 	
-	var distance = 30;
+	var distance = 31;
 	var movement = 6;
 	
-	var d = new Object();
-	d.name = "space marines";
-	d.Models = 8;
-	d.Shots = 1;
-	d.Range = 36;
-	d.Armor = 3;
-	d.BS = 4;
-	d.TOU = 4;
-	d.Cover = 0;
-	d.LD = 8;
-	d.Mob = false;
+	var b = new Object();
+	b.name = "space marines";
+	b.Models = 9;
+	b.Shots = 1;
+	b.Range = 36;
+	b.Armor = 3;
+	b.BS = 4;
+	b.TOU = 4;
+	b.Cover = 4;
+	b.LD = 8;
+	b.Mob = false;
+	b.broken = false;
+
+	b.STR = 4;
+	b.AP = 5;
 	
-	d.STR = 4;
-	d.AP = 5;
-	
-	while(a.Models > 0 && d.Models > 0) {
-		resolveShootingRound(d,a);
+	while(a.Models > 0 && b.Models > 0) {
+		resolveRound(a,b);
+		if (b.Models <=0) {
+			break;
+		}
+		resolveRound(b,a);
 		if (a.Models <=0) {
 			break;
 		}
-		resolveShootingRound(a,d);
 	}
 	stats(a);
-	stats(d);
+	stats(b);
 	if (a.Models > 0) winA++;
-	if (d.Models > 0) winB++;
+	if (b.Models > 0) winB++;
 }
 console.log("TOTALS:");
 console.log(a.name + ": " + winA);
-console.log(d.name + ": " + winB);
+console.log(b.name + ": " + winB);
 
+function resolveRound(att, def) {
+	if (att.broken) {
+		if (resolveMorale(att.Models, att)) {
+			// unit broken, falling back
+			att.broken = true;
+			distance += 6;
+			console.log("   UNIT BROKEN! retreating... distance now " + distance);
+		} else {
+			att.broken = false;
+			console.log("   Unit recovered");
+		}
+		return;
+	} else {
+		var startTotal = def.Models;
+		resolveMove(att);
+		resolveShootingRound(att,def);
+		if (def.Models <=0) {
+			return;
+		}
+		
+		if ((100*def.Models) / startTotal <= 75) {
+			if (resolveMorale(startTotal, def)) {
+				// unit broken, falling back
+				def.broken = true;
+				distance += 6;
+				console.log("   UNIT BROKEN! retreating... distance now " + distance);
+
+			}
+		}
+	}
+}
+
+function resolveMorale(start, unit) {
+	console.log("resolving Morale Check for "+ unit.name);
+	if (unit.Mob) {
+		if (unit.Models >=12) {
+			return false;
+		}
+	}
+	var r = d6() + d6();
+	console.log("   Morale " + (r > unit.LD?"failed":"safe") + ": roll=" + r);
+	return (r > unit.LD) ;
+}
+function resolveMove(unit) {
+	console.log("resolving Move Round for "+ unit.name);
+	if (unit.Cover >0) {
+		console.log("   No movement, unit in cover");
+		return;
+	}
+	if (distance <=0) {
+		console.log("   No movement, distance=0");
+		return;
+	}
+	var x = distance;
+	if (distance < 6) {
+		distance = 0;
+	} else {
+		x=6;
+		distance -= 6;
+	}
+	console.log("   moving "+ unit.Models + " " + unit.name + " "+ x + "\" closer. Distance now: " + distance);
+}
 
 function stats(o) {
 	console.log("name:   "+ o.name);
@@ -61,6 +128,10 @@ function stats(o) {
 }
 function resolveShootingRound(attacker, defender) {
 	console.log("resolving Shooting Round for "+ attacker.name);
+	if (distance > attacker.Range) {
+		console.log ("   No shooting, out of range");
+		return;
+	}
 	var hits= 0;
 	// roll attacker
 	var bsRolls = "";
@@ -92,7 +163,8 @@ function resolveShootingRound(attacker, defender) {
 	}
 	console.log("   " + defender.Models + " "+ defender.name + " took "+ wounds + " wounds");
 	defender.Models -= wounds;
-	//todo morale
+	if (defender.Models < 0) defender.Models = 0;
+
 }
 
 function d6() {
